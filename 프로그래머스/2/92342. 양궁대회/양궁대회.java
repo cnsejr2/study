1d0bb91f7944843452760ce44b9b80eca25099e9
YeongEunLee <61973941+cnsejr2@users.noreply.github.com>
2023-11-15 16:41:03 +0900

    [level 2] Title: 양궁대회, Time: 5.18 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,7 +1,12 @@
 /*
-    dfs로 풀이
-    스코어 계산
-    https://record-developer.tistory.com/124
+    풀이 출처 : https://record-developer.tistory.com/124
+    dfs를 생각함 
+        -> 모든 과녁을 방문하지만, true / false가 아닌 점수로 표시해야 한다고 생각 
+            => res를 int[] 로 설정
+    DFS :완전 탐색을 기본으로 하는 그래프 순회 기법으로 가능한 모든 경로를 탐색한다.
+        불필요한 경로를 사전에 차단하는 행동이 없다. 따라서 자원 소모가 심하다.
+    Backtracking : 경로를 찾아가는 도중에 해가 되지 않을 것 같은 경로가 갔다면 더 가지 않고 되돌아온다.
+                    불필요한 경로를 조기 차단하기 때문에 확인해야 하는 경로 수를 줄일 수 있다.
 */
 
 class Solution {
@@ -29,7 +34,7 @@ class Solution {
             }
             return;
         }
-        
+        // i과녁에 라이언이 화살을 더 많이 맞추면 반복문 종료 (백트래킹)
         for (int i = 0; i < info.length && res[i] <= info[i]; i++) {
             res[i] += 1;
             back(depth + 1, n, info);
