07c4fdf749fe718d8d3dde9523128ac81272ca03
YeongEunLee <61973941+cnsejr2@users.noreply.github.com>
2023-11-26 15:14:58 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.27 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,67 +3,44 @@
  result[0][0] += 3; 라고 적어야 했음
 */
 class Solution {
-       
+    static int[][] result = new int[4][2];
+    
+    public void sum(int c, int i, int j) {
+        if (c == 1) { result[i][j] += 3; }
+        if (c == 2) { result[i][j] += 2; }
+        if (c == 3) { result[i][j] += 1; }
+        if (c == 5) { result[i][j] += 1; }
+        if (c == 6) { result[i][j] += 2; }
+        if (c == 7) { result[i][j] += 3; }
+    }
+    
     public String solution(String[] survey, int[] choices) {
         String answer = "";
-        int[][] result = new int[4][2];
         for (int i = 0; i < survey.length; i++) {
             if (survey[i].equals("RT")) {
-                if (choices[i] == 1) { result[0][0] += 3; }
-                if (choices[i] == 2) { result[0][0] += 2; }
-                if (choices[i] == 3) { result[0][0] += 1; }
-                if (choices[i] == 5) { result[0][1] += 1; }
-                if (choices[i] == 6) { result[0][1] += 2; }
-                if (choices[i] == 7) { result[0][1] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 0, 0); }
+                if (choices[i] > 4)  { sum(choices[i], 0, 1); }
             } else if (survey[i].equals("TR")) {
-                if (choices[i] == 1) { result[0][1] += 3; }
-                if (choices[i] == 2) { result[0][1] += 2; }
-                if (choices[i] == 3) { result[0][1] += 1; }
-                if (choices[i] == 5) { result[0][0] += 1; }
-                if (choices[i] == 6) { result[0][0] += 2; }
-                if (choices[i] == 7) { result[0][0] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 0, 1); }
+                if (choices[i] > 4) { sum(choices[i], 0, 0); }
             } else if (survey[i].equals("CF")) {
-                if (choices[i] == 1) { result[1][0] += 3; }
-                if (choices[i] == 2) { result[1][0] += 2; }
-                if (choices[i] == 3) { result[1][0] += 1; }
-                if (choices[i] == 5) { result[1][1] += 1; }
-                if (choices[i] == 6) { result[1][1] += 2; }
-                if (choices[i] == 7) { result[1][1] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 1, 0); }
+                if (choices[i] > 4) { sum(choices[i], 1, 1); }
             } else if (survey[i].equals("FC")) {
-                if (choices[i] == 1) { result[1][1] += 3; }
-                if (choices[i] == 2) { result[1][1] += 2; }
-                if (choices[i] == 3) { result[1][1] += 1; }
-                if (choices[i] == 5) { result[1][0] += 1; }
-                if (choices[i] == 6) { result[1][0] += 2; }
-                if (choices[i] == 7) { result[1][0] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 1, 1); }
+                if (choices[i] > 4)  { sum(choices[i], 1, 0); }
             } else if (survey[i].equals("JM")) {
-                if (choices[i] == 1) { result[2][0] += 3; }
-                if (choices[i] == 2) { result[2][0] += 2; }
-                if (choices[i] == 3) { result[2][0] += 1; }
-                if (choices[i] == 5) { result[2][1] += 1; }
-                if (choices[i] == 6) { result[2][1] += 2; }
-                if (choices[i] == 7) { result[2][1] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 2, 0); }
+                if (choices[i] > 4) { sum(choices[i], 2, 1); }
             } else if (survey[i].equals("MJ")) {
-                if (choices[i] == 1) { result[2][1] += 3; }
-                if (choices[i] == 2) { result[2][1] += 2; }
-                if (choices[i] == 3) { result[2][1] += 1; }
-                if (choices[i] == 5) { result[2][0] += 1; }
-                if (choices[i] == 6) { result[2][0] += 2; }
-                if (choices[i] == 7) { result[2][0] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 2, 1); }
+                if (choices[i] > 4) { sum(choices[i], 2, 0); }
             } else if (survey[i].equals("AN")) {
-                if (choices[i] == 1) { result[3][0] += 3; }
-                if (choices[i] == 2) { result[3][0] += 2; }
-                if (choices[i] == 3) { result[3][0] += 1; }
-                if (choices[i] == 5) { result[3][1] += 1; }
-                if (choices[i] == 6) { result[3][1] += 2; }
-                if (choices[i] == 7) { result[3][1] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 3, 0); }
+                if (choices[i] > 4) { sum(choices[i], 3, 1); }
             } else if (survey[i].equals("NA")) {
-                if (choices[i] == 1) { result[3][1] += 3; }
-                if (choices[i] == 2) { result[3][1] += 2; }
-                if (choices[i] == 3) { result[3][1] += 1; }
-                if (choices[i] == 5) { result[3][0] += 1; }
-                if (choices[i] == 6) { result[3][0] += 2; }
-                if (choices[i] == 7) { result[3][0] += 3; }
+                if (choices[i] < 4) { sum(choices[i], 3, 1); }
+                if (choices[i] > 4) { sum(choices[i], 3, 0); }
             }
         }
 
