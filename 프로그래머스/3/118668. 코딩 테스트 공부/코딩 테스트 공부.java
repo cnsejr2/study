a591541798b3721787d87b3e38e3a76eb9433cd9
YeongEunLee <61973941+cnsejr2@users.noreply.github.com>
2023-11-29 16:23:15 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 2.69 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,57 +1,69 @@
 import java.util.*;
 
-/*
-    참고 : https://sedangdang.tistory.com/273
-*/
-
 class Solution {
-    
-    static final int INF = 1_000_000; // 대충 큰 값
-    int maxAlp; // 문제 중 가장 높은 알고력 수치
-    int maxCop; // 문제 중 가장 높은 코딩력 수치
-    
     public int solution(int alp, int cop, int[][] problems) {
-        for (int[] problem : problems) {
-            maxAlp = Math.max(maxAlp, problem[0]);
-            maxCop = Math.max(maxCop, problem[1]);
+        int targetAl = 0, targetCo = 0;
+
+        //목표 알고력과 코딩력을 구한다
+        for (int[] p : problems) {
+            targetAl = Math.max(p[0], targetAl);
+            targetCo = Math.max(p[1], targetCo);
         }
-        
-        // dp[alp][cop] = (alp, cop) 위치까지 도달하는데 걸리는 최단 시간
-        int[][] dp = new int[maxAlp + 1][maxCop + 1];
-        
-        alp = Math.min(alp, maxAlp); // alp가 이미 maxAlp보다 더 클 수 있음
-        cop = Math.min(cop, maxCop); // cop도 동일
-        
-        for (int[] ints : dp) {
-            Arrays.fill(ints, INF); // 가장 큰 값으로 배열 채우기
+
+        //목표 지점을 지나치고 능력치가 올라가는 경우, 목표 능력치로 낮춰주는 작업이 필요
+        //그렇지 않으면 목표 능력치만 맞추면 되는데 더 큰 값으로 넘어가버려서 목표 능력치에 대한 접근을 건너뛰게 됨
+
+        //두 값이 모두 목표값인 경우
+        if (alp >= targetAl && cop >= targetCo) {
+            return 0;
         }
-        
-        dp[alp][cop] = 0; // 시작 위치
-        
-        for (int i = alp; i <= maxAlp; i++) {
-            for (int j = cop; j <= maxCop; j++) {
-                // 기본 문제 풀 경우
-                if (i + 1 <= maxAlp) {
-                    dp[i + 1][j] = Math.min(dp[i+ 1][j], dp[i][j] + 1);
-                }
-                if (j + 1 <= maxCop) {
-                    dp[i][j + 1] = Math.min(dp[i][j + 1], dp[i][j] + 1);
-                }
-                // 선택 문제를 풀 경우
-                for (int[] problem : problems) {
-                    //풀 수 있는 경우
-                    if (problem[0] <= i && problem[1] <= j) {
-                        int nextAlp = Math.min(maxAlp, i + problem[2]);
-                        int nextCop = Math.min(maxCop, j + problem[3]);
-                        dp[nextAlp][nextCop] = Math.min(dp[nextAlp][nextCop], dp[i][j] + problem[4]);
+
+        //목표값보다 초기값이 큰 경우
+        if (alp >= targetAl) {
+            alp = targetAl;
+        }
+        if (cop >= targetCo) {
+            cop = targetCo;
+        }
+
+        //[알고력][코딩력] 상태에 도달하는 최단 시간 > 답은 [목표알고력][목표코딩력]
+        int[][] dp = new int[targetAl + 2][targetCo + 2];
+
+        for (int i=alp; i<=targetAl; i++) {
+            for (int j=cop; j<=targetCo; j++) {
+                dp[i][j] = Integer.MAX_VALUE; //비교를 최소화하기 위해 최대값으로 초기화
+            }
+        }
+
+        dp[alp][cop] = 0;
+
+        for(int i=alp; i<=targetAl; i++){
+            for(int j=cop; j<=targetCo; j++){
+
+                dp[i+1][j]=Math.min(dp[i+1][j],dp[i][j]+1);
+
+                dp[i][j+1]=Math.min(dp[i][j+1],dp[i][j]+1);
+
+                for(int[] p :problems){
+                    if(i>=p[0] && j>=p[1]){
+                        if(i+p[2]>targetAl && j+p[3]>targetCo){
+                            dp[targetAl][targetCo]=Math.min(dp[targetAl][targetCo],dp[i][j]+p[4]);
+                        }
+                        else if(i+p[2] > targetAl){
+                            dp[targetAl][j+p[3]]=Math.min(dp[targetAl][j+p[3]],dp[i][j]+p[4]);
+                        }
+                        else if(j+p[3] > targetCo){
+                            dp[i+p[2]][targetCo]=Math.min(dp[i+p[2]][targetCo],dp[i][j]+p[4]);
+                        }
+                        else if(i+p[2]<=targetAl && j+p[3]<=targetCo){
+                            dp[i+p[2]][j+p[3]]=Math.min(dp[i+p[2]][j+p[3]],dp[i][j]+p[4]);
+                        }
                     }
+
                 }
             }
         }
-        
-        
-        
-        return dp[maxAlp][maxCop];
-        
+
+        return dp[targetAl][targetCo];
     }
 }
