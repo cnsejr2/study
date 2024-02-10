b9a0d669fb3bf91e074dbb1a3bf36a1291fcb119
YeongEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-02-10 18:04:49 +0900

    [level 0] Title: 외계어 사전, Time: 0.04 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -2,30 +2,20 @@ import java.util.*;
 // spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 return 1;
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer = 0;
-        
         for (int i = 0; i < dic.length; i++) {
-            char[] d = dic[i].toCharArray();
-            HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-            
-            for (int k = 0; k < spell.length; k++) {
-                map.put(spell[k].charAt(0), 1); // spell에 담긴 단어를 한번씩만 사용
-            }
             boolean isFind = true;
-            for (int j = 0; j < d.length; j++) {
-                if (map.get(d[j]) != null)  {
-                    map.remove(d[j]);
-                } else { // 단어가 없는 경우 or 해당 단어를 2번 사용한 경우
-                    isFind = false; 
+            if (dic[i].length() == spell.length) {
+                for (int j = 0; j < spell.length; j++) {
+                    if (!dic[i].contains(spell[j])) {
+                        isFind = false;
+                    } 
                 }
-            }
-            if (map.size() == 0) {
                 if (isFind) {
                     return 1;
-                }
+                } 
             }
-            
         }
+        
         return 2;
     }
 }
