f3dc1c7cffdb7e893a914cfe654a1fb6b1e1a118
YeongEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-02-13 23:24:01 +0900

    [level 0] Title: 모스부호 (1), Time: 0.19 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,22 @@
+/*
+    출처 : https://nyoung-blog.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%AA%A8%EC%8A%A4%EB%B6%80%ED%98%B8-1-Java
+*/
+
+class Solution {
+    public String solution(String letter) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.",
+                "--.","....","..",".---","-.-",".-..","--","-.",
+                "---",".--.","--.-",".-.","...","-","..-","...-",
+                ".--","-..-","-.--","--.."};
+        String[] morseString;
+        morseString = letter.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+        for (String word : morseString) {
+            for (int i = 0; i < morse.length; i++) {
+                if (word.equals(morse[i])) sb.append(Character.toString(i + 'a'));
+            }
+        }
+        return sb.toString();
+    }
+}
