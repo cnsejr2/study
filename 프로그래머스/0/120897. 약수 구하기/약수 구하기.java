bb29e6779ea50911b473914e40c087126e4b9ae5
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-03-26 10:59:11 +0900

    [level 0] Title: 약수 구하기, Time: 0.22 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
