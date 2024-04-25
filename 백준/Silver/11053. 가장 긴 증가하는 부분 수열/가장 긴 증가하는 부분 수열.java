a7a82523e931384153e75ca885420e4e67a6e3fa
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-04-25 22:54:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 272 ms, Memory: 20536 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,51 +1,47 @@
 import java.util.Scanner;
- 
+
 public class Main {
 	
-	static int[] seq;
-	static Integer[] dp;
-	
+	static int[] arr;
+	static int[] dp;
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		
-		Scanner in = new Scanner(System.in);
+		int n = sc.nextInt();
+		arr = new int[n];
+		dp = new int[n];
 		
-		int N = in.nextInt();
-		
-		seq = new int[N];
-		dp = new Integer[N];
-		
-		
-		for(int i = 0; i < N; i++) {
-			seq[i] = in.nextInt();
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
 		}
 		
-		// 0 ~ N-1 까지 모든 부분수열 탐색 
-		for(int i = 0; i < N; i++) {
-			LIS(i);
+		for (int i = 0; i < n; i++) {
+			findArr(i);
 		}
 		
 		int max = dp[0];
-		// 최댓값 찾기 
-		for(int i = 1; i < N; i++) {
+		for (int i = 0; i < n; i++) {
 			max = Math.max(max, dp[i]);
 		}
 		System.out.println(max);
-	}
-	
-	
-	static int LIS(int N) {
 		
-		// 만약 탐색하지 않던 위치의 경우 
-		if(dp[N] == null) {
-			dp[N] = 1;	// 1로 초기화 
+		sc.close();
+	}
+
+	private static int findArr(int n) {
+		// TODO Auto-generated method stub
+		if (dp[n] == 0) {
+			dp[n] = 1;
 			
-			// N-1 부터 0까지중 N보다 작은 값들을 찾으면서 재귀호출. 
-			for(int i = N - 1; i >= 0; i--) {
-				if(seq[i] < seq[N]) {
-					dp[N] = Math.max(dp[N], LIS(i) + 1);
+			for (int i = n - 1; i >= 0; i--) {
+				if (arr[i] < arr[n]) {
+					dp[n] = Math.max(dp[n], findArr(i) + 1);
 				}
 			}
 		}
-		return dp[N];
+		return dp[n];
 	}
+
 }
