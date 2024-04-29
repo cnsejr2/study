c13b457d8b78b0667ab7932543d4b7948ecf4a82
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-04-30 01:45:49 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,19 +1,11 @@
 class Solution {
     public int solution(int chicken) {
         int answer = 0;
-        int remain = 0;
-        while (chicken > 0) {
+        
+        while (chicken / 10 > 0) {
             answer += chicken / 10;
-            remain += chicken % 10;
-            chicken /= 10;
+            chicken = chicken / 10 + chicken % 10;
         }
-        chicken = remain;
-        remain = 0;
-        while (chicken > 0) {
-            answer += chicken / 10;
-            remain += chicken % 10;
-            chicken /= 10;
-        }
-        return answer + (remain / 10);
+        return answer;
     }
 }
