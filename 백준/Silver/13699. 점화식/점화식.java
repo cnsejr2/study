2ae594e35520f71245da31c02096b1bc68b6c20f
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-04-30 20:25:56 +0900

    [Silver IV] Title: 점화식, Time: 212 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		long[] dp = new long[36];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int i = 2; i < 36; i++) {
+			for (int j = 0; j < i; j++) {
+				dp[i] += (dp[j] * dp[i - 1 - j]);
+			}
+		}
+		
+		System.out.println(dp[n]);
+		
+		sc.close();
+	}
+
+}
