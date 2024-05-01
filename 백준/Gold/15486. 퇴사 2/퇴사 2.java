42e605b8f945491b7214fc2fd264fcd568a49675
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-05-01 09:02:09 +0900

    [Gold V] Title: 퇴사 2, Time: 2664 ms, Memory: 390816 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[][] arr = new int[n + 2][2];
+		int[] dp = new int[n + 2];
+		
+		for (int i = 1; i <= n; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+	
+		
+		int max = -1;
+		for (int i = 1; i <= n+1; i++) {
+			if (max < dp[i]) {
+				max = dp[i];
+			}
+			
+			int nxt = i + arr[i][0];
+			if (nxt < n + 2) {
+				dp[nxt] = Math.max(dp[nxt], max + arr[i][1]);
+			}
+		}
+		System.out.println(dp[n + 1]);
+		
+		sc.close();
+
+	}
+
+}
