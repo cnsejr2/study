63c5fba583fb312b9410942bbd6611703e43b23d
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-05-03 08:49:17 +0900

    [Silver IV] Title: 스택, Time: 444 ms, Memory: 35488 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static int[] stack;
+	static int size = 0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int n = sc.nextInt();
+		
+		stack = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
+			
+			switch (str) {
+				case "push":
+					push(sc.nextInt());
+					break;
+				case "pop":
+					sb.append(pop()).append('\n');
+					break;
+				case "size":
+					sb.append(size()).append('\n');
+					break;
+				case "empty":
+					sb.append(empty()).append('\n');
+					break;
+				case "top":
+					sb.append(top()).append('\n');
+					break;
+				
+			}
+		}
+		
+		
+		System.out.println(sb);
+		
+		
+		sc.close();
+		
+	}
+	
+	public static int top() {
+		if (size == 0) {
+			return -1;
+		}  else {
+			return stack[size - 1];
+		}
+	}
+	
+	public static int empty() {
+		if (size == 0) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+	
+	public static int size() {
+		return size;
+	}
+
+	public static int pop() {
+		// TODO Auto-generated method stub
+		if (size == 0) {
+			return -1;
+		} else {
+			int res = stack[size - 1];
+			stack[size - 1] = 0;
+			size--;
+			return res;
+		}
+	}
+
+	public static void push(int n) {
+		// TODO Auto-generated method stub
+		stack[size] = n;
+		size++;
+	}
+	
+	
+	
+
+}
