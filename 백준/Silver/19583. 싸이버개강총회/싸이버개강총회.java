71790c179c230b77b078ddde8f3ece78f33a0191
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-06-30 20:37:10 +0900

    [Silver II] Title: 싸이버개강총회, Time: 440 ms, Memory: 54832 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -1,44 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    String s, e, q;
+    Set<String> S = new HashSet<>();
+    int ans = 0;
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		String start, end, last;
-		Set<String> check = new HashSet<>();
-		int count = 0;
-		
-		st = new StringTokenizer(br.readLine());
-		start = st.nextToken();
-		end = st.nextToken();
-		last = st.nextToken();
-		
-		while (true) {
-			String input = br.readLine();
-			if (input == null || input.length() == 0) {
-				break;
-			}
-			
-			String[] str = input.split(" ");
-			if (str[0].compareTo(start) <= 0) {
-				check.add(str[1]);
-			} else if (str[0].compareTo(end) >= 0 && str[0].compareTo(last) <= 0) {
-				if (check.contains(str[1])) {
-					count++;
-					check.remove(str[1]);
-				}
-			}
-		}
-		System.out.println(count);
-		
-	
-	}
+    void solution() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        s = st.nextToken();
+        e = st.nextToken();
+        q = st.nextToken();
+        String input;
+        while ((input = br.readLine()) != null) {
+            String[] str = input.split(" ");
+            if (str[0].compareTo(s) <= 0)
+                S.add(str[1]);
+            else if (str[0].compareTo(e) >= 0 && str[0].compareTo(q) <= 0) {
+                if (S.contains(str[1])) {
+                    ans++;
+                    S.remove(str[1]);
+                }
+            }
+        }
+        System.out.println(ans);
+    }
 
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
 }
