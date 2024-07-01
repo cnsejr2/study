e3da1859461f27b345c529cbcff5d69faa06b461
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-07-01 09:54:17 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 204 ms, Memory: 19264 KB -BaekjoonHub

---
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        boolean flag = false;
+        int len = s.length();
+        
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                System.out.println(s.length());
+                return;
+            } else if (s.charAt(i) != s.charAt(i + 1)) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            System.out.println(s.length() - 1);
+        } else {
+            System.out.println(-1);
+        }
+        
+        
+        
+    }
+}
