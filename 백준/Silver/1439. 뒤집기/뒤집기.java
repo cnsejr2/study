4cf90be49d9e60cf4bf46810baf81c7d1a7782d1
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-07-01 21:17:31 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String data = br.readLine();
+		
+		int answer = 0;
+		int index = 1;
+		
+		if (data.length() > 0) {
+			char std = data.charAt(0);
+			
+			while (index < data.length()) {
+				if (data.charAt(index) == std) {
+					index++;
+					continue;
+				}
+				
+				answer++;
+				for (int i = index; i < data.length(); i++) {
+					if (data.charAt(index) == std) {
+						break;
+					}
+					index++;
+				}
+			}
+		}
+		System.out.println(answer);
+
+	}
+
+}
