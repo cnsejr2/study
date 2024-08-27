8c6ecd4e480e0d942303009e580fca23b9bd8ff1
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-08-27 16:01:23 +0900

    [Silver III] Title: 두 수의 합, Time: 776 ms, Memory: 104064 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -3,41 +3,33 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		Arrays.sort(arr);
-		
-		int x = sc.nextInt();
-		int cnt = 0;
-		int left = 0;
-		int right = n - 1;
-		
-		while (left < right) {
-			int sum = arr[left] + arr[right];
-			if (sum == x) {
-				cnt++;
-			}
-			
-			if (sum < x) {
-				left++;
-			} else {
-				right--;
-			}
-		}
-		
-		
-		System.out.println(cnt);
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
 
+        Arrays.sort(arr);
+
+        int start = 0;
+        int end = n - 1;
+        int cnt = 0;
+        while (start < end) {
+            int num = arr[start] + arr[end];
+            if (num < x) {
+                start++;
+            } else if (num > x) {
+                end--;
+            } else {
+                cnt++;
+                start++; end--;
+            }
+        }
+        System.out.println(cnt);
+
+        sc.close();
+    }
 }
