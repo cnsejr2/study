0adba1a86a88df1d7c6c1e76aabc984259a291b7
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-09-08 16:10:38 +0900

    [Silver I] Title: 경로 찾기, Time: 328 ms, Memory: 20552 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -4,34 +4,34 @@ import java.io.InputStreamReader;
 
 public class Main {
 	
-	static final int INF = 10000000;
-	static int[][] cost = new int[101][101];
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
+		int[][] cost = new int[n + 1][n + 1];
+		
 		for (int i = 1; i <= n; i++) {
 			String str = br.readLine();
 			str = str.replaceAll(" ", "");
 			for (int j = 1; j <= n; j++) {
-				cost[i][j] = str.charAt(j - 1) == '1' ? 1 : INF;
+				cost[i][j] = str.charAt(j - 1) == '1' ? 1 : 0;
 			}
-			
 		}
 		
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				for (int k = 1; k <= n; k++) {
-					cost[j][k] = Math.min(cost[j][k], cost[j][i] + cost[i][k]);
+					if (cost[j][i] == 1 && cost[i][k] == 1) {
+						cost[j][k] = 1;
+					}
 				}
 			}
 		}
 		
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
-				cost[i][j] = cost[i][j] == INF ? 0 : 1;
 				System.out.print(cost[i][j] + " ");
 			}
 			System.out.println();
