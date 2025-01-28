6cc3fe914bb3c08b4a05c4f744cfa19771491f23
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2025-01-28 17:31:28 +0900

    [Silver III] Title: N과 M (3), Time: 588 ms, Memory: 221216 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, m;
+	static ArrayList<Integer> list = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		dfs(0);
+		
+		System.out.println(sb);
+			
+	}
+
+	private static void dfs(int depth) {
+		// TODO Auto-generated method stub
+		if (m == depth) {
+			for (int i = 0; i < list.size(); i++) {
+				sb.append(list.get(i) + " ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for (int i = 1; i <= n; i++) {
+			list.add(i);
+			dfs(depth + 1);
+			list.remove(list.size() - 1);
+		}
+	}
+	
+	
+
+}
