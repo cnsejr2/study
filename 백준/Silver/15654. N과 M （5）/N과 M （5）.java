5dbe335017695fbf10957db8ac6006eb618e03a9
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2025-01-28 17:57:17 +0900

    [Silver III] Title: N과 M (5), Time: 472 ms, Memory: 59320 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, m;
+	static ArrayList<Integer> arr = new ArrayList<>();
+	static ArrayList<Integer> list = new ArrayList<>();
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr.add(Integer.parseInt(st.nextToken()));
+		}
+		Collections.sort(arr);
+		
+		dfs(0);
+		
+		System.out.println(sb);
+
+	}
+	
+	private static void dfs(int depth) {
+		
+		if (m == depth) {
+			for (int i = 0; i < list.size(); i++) {
+				sb.append(list.get(i) + " ");
+			}
+			sb.append('\n');
+			return;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			if (!list.contains(arr.get(i))) {
+				list.add(arr.get(i));
+				dfs(depth + 1);
+				list.remove(list.size() - 1);	
+			}
+		}
+		
+	}
+
+}
