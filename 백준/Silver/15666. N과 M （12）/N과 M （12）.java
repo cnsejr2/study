aa17668313b9b23035587d03838168a239b0b2d2
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2025-01-29 16:10:13 +0900

    [Silver II] Title: N과 M (12), Time: 216 ms, Memory: 25008 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, m;
+	static ArrayList<Integer> list = new ArrayList<>();
+	static Set<String> sList = new HashSet<>();
+	static HashMap<Integer, Integer> map = new HashMap<>();
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < n; i++) {
+			map.put(i, arr[i]);
+		}
+		
+		dfs(0, 0);
+		
+		System.out.println(sb);
+
+	}
+	
+	private static void dfs(int s, int depth) {
+		if (m == depth) {
+			String str = "";
+			for (int i = 0; i < list.size(); i++) {
+				str += map.get(list.get(i)) + " ";
+			}
+			
+			if (!sList.contains(str)) {
+				sList.add(str);
+				sb.append(str + '\n');
+			}
+			return;
+		}
+		
+		for (int i = s; i < n; i++) {
+			list.add(i);
+			dfs(i, depth + 1);
+			list.remove(list.size() - 1);
+		}
+		
+	}
+	
+
+}
